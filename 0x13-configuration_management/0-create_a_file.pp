5fde0947f738b6d535296ff41a5d526e20e2dc6e
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-21 20:10:47 +0000

    add comment

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
+# create a file
 file { '/tmp/holberton':
-  ensure => file,
-  path   => '/tmp/holberton',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
