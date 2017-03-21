462c00557ecce03a3d8a8e74497241b266304d58
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-21 19:34:18 +0000

    use single quotes instead of double

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { "/tmp/holberton" :
+file { '/tmp/holberton':
   ensure => file,
-  path   => "/tmp/holberton",
-  mode   => "0744",
-  owner  => "www-data",
-  group  => "www-data"
+  path   => '/tmp/holberton',
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data'
 }
