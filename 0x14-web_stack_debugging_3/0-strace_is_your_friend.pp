cf5a13826353be027d352f0e82ebb6d9957061c5
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:25:29 +0000

    add extra path

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Run sed to edit line in file 'wp-settings.php'.
 exec { "sed":
-  command => "sed -ni 's/locale.phpp/locale.php/' wp-settings.php",
+  command => "sed -i 's/locale.phpp/locale.php/' wp-settings.php",
   cwd     => "/var/www/html",
-  path    => ["/usr/bin", "/usr/sbin"]
+  path    => ["/bin", "/usr/bin", "/usr/sbin"]
 }
