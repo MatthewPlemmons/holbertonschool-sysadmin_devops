c337592502b26982057dca3f9ed5dbc21d414d7c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:31:48 +0000

    add flag to sed

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Run sed to edit line in file 'wp-settings.php'.
 exec { "sed":
-  command => "sed -i 's/locale.phpp/locale.php/' /var/www/html/wp-settings.php",
+  command => "sed -ie 's/locale.phpp/locale.php/' /var/www/html/wp-settings.php",
   path    => ["/bin", "/usr/bin", "/usr/sbin"]
 }
