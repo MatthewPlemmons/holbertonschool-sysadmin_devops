8e035cb055bcd70bbdf6bd7220a7b4d98210eb32
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:14:10 +0000

    add command attribute back

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Run sed to edit line in file 'wp-settings.php'.
-exec { "sed -i 's/locale.phpp/locale.php/' wp-settings.php":
-  cwd     => '/var/www/html',
-  path    => ['/usr/bin', '/usr/sbin']
+exec { "sed":
+  command => "sed -ni 's/locale.phpp/locale.php/' wp-settings.php",
+  cwd     => "/var/www/html",
+  path    => ["/usr/bin", "/usr/sbin"]
 }
