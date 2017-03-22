b6ef0dc106c76f67116d8147c2b15f018f819df6
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 04:53:16 +0000

    add path attribute

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Run sed to edit line in file 'wp-settings.php'.
 exec { "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' wp-settings.php":
   command => 'sed',
-  cwd     => '/var/www/html'
+  cwd     => '/var/www/html',
+  path    => ['/usr/bin', '/usr/sbin']
 }
