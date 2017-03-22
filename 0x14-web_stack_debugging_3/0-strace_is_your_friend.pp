34f92b08ca7dc2dbae6fe4ab1179bad912393022
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:10:46 +0000

    remove an attribute

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Run sed to edit line in file 'wp-settings.php'.
 exec { "sed -i 's/locale.phpp/locale.php/' wp-settings.php":
-  command => 'sed',
   cwd     => '/var/www/html',
   path    => ['/usr/bin', '/usr/sbin']
 }
