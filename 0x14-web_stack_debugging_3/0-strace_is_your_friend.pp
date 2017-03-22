8c2cdb51877b9c7dfd89fe48178d793c09516a02
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:29:16 +0000

    edit command attribute

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Run sed to edit line in file 'wp-settings.php'.
 exec { "sed":
-  command => "sed -i 's/locale.phpp/locale.php/' wp-settings.php",
+  command => "sed -i 's/locale.phpp/locale.php/' /var/www/html/wp-settings.php",
   cwd     => "/var/www/html",
   path    => ["/bin", "/usr/bin", "/usr/sbin"]
 }
