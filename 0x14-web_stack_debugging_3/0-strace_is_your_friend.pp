a7123978578be3361168bbb761fd71e2e4c78f9a
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-22 05:38:39 +0000

    add cwd attribute

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # run sed to edit line in file wp-settings.php
 exec { 'sed':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  command => "sed -i 's/phpp/php/' wp-settings.php",
+  cwd     => '/var/www/html',
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
