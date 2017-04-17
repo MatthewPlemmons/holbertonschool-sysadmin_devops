0620c52a6a513245c21b4b1f310a0694314ad324
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-17 22:53:19 +0000

    update working task 0 script

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -6,12 +6,15 @@ import requests
 
 
 if __name__ == "__main__":
-    #params = {'completed': 'true'}
-    url = ('http://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        sys.argv[1]))
-    r = requests.get(url)
-    #import pdb; pdb.set_trace()
-    json_data = r.json()
-    
-    for todo in json_data:
-        print(todo['title'])
+    r_todo = requests.get('http://jsonplaceholder.typicode.com/users/{}/todos'
+                          .format(sys.argv[1]))
+    r_user = requests.get('http://jsonplaceholder.typicode.com/users/{}'
+                          .format(sys.argv[1]))
+    json_todo = r_todo.json()
+    fin = [x for x in json_todo if x['completed'] is True]
+    employee_name = r_user.json()['name']
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+        employee_name, len(fin), len(json_todo)))
+    for n in fin:
+        print("\t{}".format(n['title']))
