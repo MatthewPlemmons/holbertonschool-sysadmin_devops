b087b5b320ca16cf4c15d428ff556c0c16a14c6e
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-18 02:07:33 +0000

    remove a space from printed message

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     r_todo = requests.get(url + '{}/todos'.format(sys.argv[1])).json()
     r_user = requests.get(url + '{}'.format(sys.argv[1])).json()
     fin = [x for x in r_todo if x.get('completed') is True]
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         r_user.get('name'), len(fin), len(r_todo)))
     for n in fin:
         print("\t{}".format(n.get('title')))
