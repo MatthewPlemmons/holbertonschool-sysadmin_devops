cb7daceebde01e98aa6d3a117d9764cc316c7b6b
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-18 00:18:27 +0000

    update task 1

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """Export json data to a CSV file."""
-
-import sys
+from sys import argv
 import requests
 import csv
 
 
 if __name__ == "__main__":
-    r_todo = requests.get('http://jsonplaceholder.typicode.com/users/{}/todos'
-                          .format(sys.argv[1]))
-    r_user = requests.get('http://jsonplaceholder.typicode.com/users/{}'
-                          .format(sys.argv[1]))
+    url = 'http://jsonplaceholder.typicode.com/users/'
+    r_todo = requests.get(url + '{}/todos'.format(argv[1]))
+    r_user = requests.get(url + '{}'.format(argv[1]))
     json_todo = r_todo.json()
     username = r_user.json()['username']
-    with open('{}.csv'.format(user_id), 'w') as f:
+    with open('{}.csv'.format(json_todo[0]['userId']), 'w') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for n in json_todo:
-            data = [n['userId'], username, n['completed'], n['title']]
-            writer.writerow(data)
+            writer.writerow([n['userId'], username, n['completed'], n['title']])
