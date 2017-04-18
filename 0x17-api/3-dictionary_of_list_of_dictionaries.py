5c11543b898bbb86fad667c24753a25d8326d74c
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-18 04:26:26 +0000

    exports json data to file

---
+++ 0x17-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Export data in JSON format to file."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://jsonplaceholder.typicode.com/'
+    todos = requests.get(url + 'todos').json()
+    users = requests.get(url + 'users').json()
+    data = {}
+    import pdb; pdb.set_trace()
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        data.update({str(user_id): []})
+        for t in (task for task in todos if task.get('userId') == user_id):
+            data[str(user_id)].append({"task": t.get('title'),
+                                       "completed": t.get('completed'),
+                                       "username": username})
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
