1082f20f4df99517b51bd9b5e23152653339cf5e
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-18 02:05:18 +0000

    update imports to pass checker

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """Script to return user info based on given ID number"""
-from sys import argv
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = 'http://jsonplaceholder.typicode.com/users/'
-    r_todo = requests.get(url + '{}/todos'.format(argv[1]))
-    r_user = requests.get(url + '{}'.format(argv[1]))
-    json_todo = r_todo.json()
-    fin = [x for x in json_todo if x.get('completed') is True]
+    r_todo = requests.get(url + '{}/todos'.format(sys.argv[1])).json()
+    r_user = requests.get(url + '{}'.format(sys.argv[1])).json()
+    fin = [x for x in r_todo if x.get('completed') is True]
     print("Employee {} is done with tasks ({}/{}):".format(
-        r_user.json().get('name'), len(fin), len(json_todo)))
+        r_user.get('name'), len(fin), len(r_todo)))
     for n in fin:
         print("\t{}".format(n.get('title')))
