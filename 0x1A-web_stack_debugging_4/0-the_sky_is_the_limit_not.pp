7be0b801da0bb5aaef71b66ad2261a935ed92100
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-05-27 06:45:27 +0000

    format correction for puppet-lint

--- 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # puppet script to configure nginx
 exec { '/etc/default/nginx':
-     command => '/bin/echo "ULIMIT=\'-n 4096\'" > /etc/default/nginx &&
-                /etc/init.d/nginx restart',
+  command => '/bin/echo "ULIMIT=\'-n 4096\'" > /etc/default/nginx &&
+  /etc/init.d/nginx restart',
 }
