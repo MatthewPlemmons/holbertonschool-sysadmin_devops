c56165012a85d7ed8c655b071592de2090602d28
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-05-27 06:39:53 +0000

    add puppet script

---
+++ 0x1A-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# puppet script to configure nginx
+exec { '/etc/default/nginx':
+     command => '/bin/echo "ULIMIT=\'-n 4096\'" > /etc/default/nginx &&
+                /etc/init.d/nginx restart',
+}
